c451ac703c7c16ecc45a29a48c92e1a4ddd07c0a
Shota Shimazu <hornet.live.mf@gmail.com>
2018-11-09 17:44:18 +0900

    revise

--- apib/models.py
+++ apib/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class APIB(models.Model):
+    division = models.DateTimeField()
+    content = models.TextField()
+    is_disabled = models.BooleanField()
--- apib/views.py
+++ apib/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+class APIRef(View):
+
+    @login_required
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
