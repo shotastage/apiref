98f6aed1c785ba72bc44412b38ca46ace7fa4228
Shota Shimazu <hornet.live.mf@gmail.com>
2019-06-17 10:17:01 +0900

    add view

--- setup/views.py
+++ setup/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from urllib.parse import quote
+from django.views.generic import View
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.models import User
+from account.models import InvitationCode
 
-# Create your views here.
+
+
+class CodeGenerate(View):
+
+    def get(self, request):
+        return render(request, 'registration/code_result.html')
+
+
+    @method_decorator(login_required)
+    def post(self, request):
+
+        user = request.POST["user"]
+        email = request.POST["email"]
+        passwd = request.POST["passwd"]
+        passwd_confirm = request.POST["passwd_confirm"]
+
+
+        if passwd == passwd_confirm:
+            pass
+
+        res = redirect("/register_new/")
+        res['location'] += '?' "email=" + quote(email)
+
+        return res
