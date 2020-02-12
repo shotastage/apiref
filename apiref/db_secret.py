73cdee6759c45201e712a269dc68c856f3b7726c
Shota Shimazu <hornet.live.mf@gmail.com>
2020-02-12 11:37:16 +0900

    revise

--- apiref/db_secret.py
+++ apiref/db_secret.py
@@ -2,15 +2,24 @@ import os, sys
 
 
 try:
-    database_setting = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ["APIREF_DB_NAME"],
-            'USER': os.environ["APIREF_DB_USER"],
-            'PASSWORD': os.environ["APIREF_DB_PASS"],
-            'HOST': os.environ["APIREF_DB_HOST"],
-            'PORT': '5432',
+
+    if "postgres://" in os.environ["DATABASE_URL"]:
+
+        import dj_database_url
+
+        database_setting = {
+            'default': dj_database_url.config(default=os.environ["DATABASE_URL"])
         }
-    }
+    else:
+        database_setting = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': os.environ["APIREF_DB_NAME"],
+                'USER': os.environ["APIREF_DB_USER"],
+                'PASSWORD': os.environ["APIREF_DB_PASS"],
+                'HOST': os.environ["APIREF_DB_HOST"],
+                'PORT': '5432',
+            }
+        }
 except:
     database_setting = {}
